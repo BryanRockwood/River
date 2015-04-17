a6742903ac80d7daa09e8783843ba97a75f781d8
Matthew Musich <mcr1221@hotmail.com>
2015-04-17 19:56:03 -0400

    doesnt work...

--- test.scala
+++ test.scala
@@ -1,19 +1,15 @@
 object RiverPrint {
 
 	def main(args: Array[String]){
-	  var test = Array(1 to 30)
+	  var test:Array[Int] = Array(1 to 30)
 
 
 	  for (x <- test){
 	  	println(x)
 	  	Thread sleep 500
-	  	var inpt = readLine()
-	  	if(inpt != null){
-	  		println("got text")
-	  	}
+	  	
 	  }
-
 	}
-	
-	
 }
+	
+	
