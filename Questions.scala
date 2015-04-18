4c33e3ac0652df3cee3712c3bf61e708edcb37fc
Bryan Rockwood <bryanprockwood@gmail.com>
2015-04-17 23:30:34 -0400

    Revert "Woah BRO"
    This reverts commit 56a820b5bbce0e4196d920eeca4dab49575bf527.

--- Questions.scala
+++ Questions.scala
@@ -16,33 +16,5 @@ Who are you when you arent hungry?
 
 When the bell rings, who will answer the gong?
 
-Describe your thoughts when you first saw the lights.
-
-How would you know if you were really there?
-
-How was the ride?
-
-When you open the gates, who will greet you?
-
-Who has become the pantaloon?
-
-Who let the dogs out?
-
-When Baal fought the 7 gods out of Hillsguard, where was Torvir?
-
-When a heart ceases to beat, has the music come to an end? or merely a pause?
-
-What if you blink, and you change your conciousness?
-
-What are you accomplishing in another life?
-
-Look to your left, what ghost resides on your shoulder?
-
-How many licks does it take to get to the center?
-
-Is deja vu you just reloading from a checkpoint?
-
-You will never see the entire planet with your own eyes.
-
 
 
