8c2775986d10dbad5ccfa3673d89784516523c7c
Bryan Rockwood <bryanprockwood@gmail.com>
2015-04-17 22:38:12 -0400

    Questions to ponder
    But wahy?

---
+++ Questions.scala
@@ -0,0 +1,11 @@
+What does red taste like?
+
+What if you were merely a brain in a vat, creating memories for a computer?
+
+If you acted on a cause, was it your choice? or destined?
+
+When does boldness rival brashness?
+
+If the chicken lays the egg, when does the crow caw?
+
+ 
