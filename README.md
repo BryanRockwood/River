190e3b6598ce3582ca86db5ed1a13b105a9508b3
Matthew Musich <mcr1221@hotmail.com>
2015-04-19 11:21:07 -0400

    Added restrictions

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Influenced by Mountain
 The only File needed to run is River.scala
 
 By: Bryan Rockwood and Matt Musich
+
+Restrictions:
+//No Descriptive Function Names - $300
+//No Indentation - $300
+//No Comments - $300
+//Everything Must Be Included In One File - $200
--- River.scala
+++ River.scala
@@ -1,10 +1,4 @@
 object RiverPrint {
-
-//No Descriptive Function Names - $300
-//No Indentation - $300
-//No Comments - $300
-//Everything Must Be Included In One File - $200
-
 def main(args: Array[String]){
 println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 println("██████╗ ██╗██╗   ██╗███████╗██████╗ ")
